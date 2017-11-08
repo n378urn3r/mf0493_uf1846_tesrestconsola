90902c8b58789a4e1a02a6ca1b3f18e9e34c2830
JavierHdez <jhregalado@googlemail.com>
2017-11-08 20:05:58 +0100

    primer commit

---
+++ TestRestConsola/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+using System.Collections.Generic;
+
+using System.Net.Http;
+
+using System.Net.Http.Headers;
+
+using System.Runtime.Serialization.Json;
+
+using System.Threading.Tasks;
+
+
+
+namespace TestRestConsola
+
+{
+
+    class Program
+
+    {
+
+        static void Main(string[] args)
+
+        {
+
+            var repositories = ProcessRepositories().Result;
+
+
+
+            foreach (var repo in repositories)
+
+            {
+
+                Console.WriteLine(repo.name);
+
+                //Console.WriteLine(repo.Description);
+
+                //Console.WriteLine(repo.GitHubHomeUrl);
+
+                //Console.WriteLine(repo.Homepage);
+
+                //Console.WriteLine(repo.Watchers);
+
+                //Console.WriteLine(repo.LastPush);
+
+                //Console.WriteLine();
+
+            }
+
+        }
+
+
+
+        private static async Task<List<repo>> ProcessRepositories()
+
+        {
+
+            var client = new HttpClient();
+
+            var serializer = new DataContractJsonSerializer(typeof(List<repo>));
+
+
+
+            client.DefaultRequestHeaders.Accept.Clear();
+
+            client.DefaultRequestHeaders.Accept.Add(
+
+                new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
+
+            client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
+
+
+
+          //  var stringTask = client.GetStringAsync("https://api.github.com/orgs/dotnet/repos");
+
+            var streamTask = client.GetStreamAsync("https://api.github.com/orgs/dotnet/repos");
+
+            var repositories = serializer.ReadObject(await streamTask) as List<repo>;
+
+            return repositories;
+
+        }
+
+    }
+
+}
